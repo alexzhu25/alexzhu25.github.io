1c8d5c21c2451e3279cb3449d6e19f936af58844
alexzhu25 <alexzhu25@gmail.com>
2017-06-08 09:04:35 -0700

    added to codeexamples page
    added more sections to codeexamples page, changed title css

--- scripts/jsExamples.js
+++ scripts/jsExamples.js
@@ -1,12 +1,53 @@
+/* takes input year born and calculates age */
 function calcAge() {
 	var inputYear = $("#yearInput").val();
+	if (inputYear == "") {
+		return;
+	}
 	var age = 2017 - inputYear;
-	$("#yearResult").text("Result: " + age);
+	$("#ageResult").text("Result: " + age + " years old");
+}
+
+/* if button is clicked, moves img or text back and forth */
+/* loop for back and forth movement, needs break method
+function animateImg() {
+	var elem = document.getElementById("animatedImg"); 
+	var pos = 0;
+	var id = setInterval(frameForward, 1);
+	function frameForward() {
+		if (pos == 300) {
+			clearInterval(id);
+			var id = setInterval(frameBack, 1);
+		} 
+		else {
+			pos++; 
+			elem.style.left = pos + 'px'; 
+		}
+	}
+	function frameBack() {
+		if (pos == 0) {
+			clearInterval(id);
+			var id = setInterval(frameForward, 1);
+		} 
+		else {
+			pos--; 
+			elem.style.left = pos - 'px'; 
+		}
+	}
+}
+*/
+
+/* if button is clicked, moves text around and changes size */
+function animateImg() {
+	var elem = $("#animatedImg");
+	elem.animate({left: "300px", fontSize: "3em"}, 1000);
+	elem.animate({left: "0px", fontSize: "1em"}, 1000);
 }
 
 $(document).ready(function() {
 	$("#button1").click(function(){
 		$(this).next().slideToggle("fast");
 	});
-	$("#yearButton").click(calcAge);
+	$("#ageButton").click(calcAge);
+	$("#animateButton").click(animateImg);
 });
